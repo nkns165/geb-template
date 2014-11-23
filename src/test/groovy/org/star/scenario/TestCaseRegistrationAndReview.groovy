4af047a19d3f0ee6d8c800b765e83d60c1584751
kenichiro.ota <kenichiro.ota@shiftinc.jp>
2014-11-23 15:52:34 +0900

    テストケースの登録、レビューのシナリオを追加

---
+++ src/test/groovy/org/star/scenario/TestCaseRegistrationAndReview.groovy
@@ -0,0 +1,14 @@
+package org.star.scenario
+
+import geb.spock.GebReportingSpec
+
+
+/**
+ * Created by kenichiro_ota on 14/11/23.
+ */
+class TestCaseRegistrationAndReview extends GebReportingSpec {
+    def "新規タグと関連づけたテストケースを下僕が起票して、先生がレビューして修正し、下僕に確認させる" () {
+       expect:
+       true
+    }
+}
