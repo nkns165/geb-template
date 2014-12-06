5001ead07f86ef72ffaa750fbd05505a06f5dc23
May <may.pptx@gmail.com>
2014-12-06 22:38:00 +0900

    TestCaseページでTagNameをクリックすると、TagNameが設定されているTestCaseのみ表示されるテストを作成

--- src/test/groovy/org/star/scenario/WhenFilteringTestCase.groovy
+++ src/test/groovy/org/star/scenario/WhenFilteringTestCase.groovy
@@ -14,16 +14,17 @@ class WhenFilteringTestCase extends GebReportingSpec {
     String tagName
     String testCaseName
     String testCaseScenario
+    String tagDescription
     User admin
 
     def setup() {
         tagName = "TagName_" + UUID.randomUUID()
+        tagDescription = "description_" + UUID.randomUUID()
         testCaseName = "TestCase_" + UUID.randomUUID()
         testCaseScenario = "Scenario" + UUID.randomUUID()
         admin = new User(userName: "admin", password: "admin", mailAddress: "stac2014tamagawa@gmail.com", mailPassword: "tamagawa2014", browser: browser)
     }
 
-    //作成中
     def "TestCaseページでTagNameをクリックすると、TagNameが設定されているTestCaseのみ表示される"() {
         when: "ログインする"
         to TopPage
@@ -35,12 +36,11 @@ class WhenFilteringTestCase extends GebReportingSpec {
         then: "Tagページが表示される"
         at TagPage
         when: "Tagを追加する"
-        createTag(tagName, "説明")
+        createTag(tagName, tagDescription)
         then: "Tagが追加される"
         TagCreationIsSuccessful()
         when: "TestCaseページを開く"
         header.testCase.click()
-        //to TestCasePage
         then: "TestCaseページが表示される"
         at TestCasePage
         when: "作成したTagを指定したTestCaseを追加する"
@@ -55,7 +55,9 @@ class WhenFilteringTestCase extends GebReportingSpec {
         when: "TestCaseリストから最初に追加したTestCaseのTagNameリンクをクリックする"
         header.testCase.click()
         filterByTag("Equal To", tagName)
-        then: "TestCase詳細ページに遷移する"
-        and: "TestCase詳細ページに作成した2件のTestCaseのみ表示されていることを確認する"
+        then: "TestCase詳細ページに作成した2件のTestCaseのみ表示されていることを確認する"
+        testCaseItems[0].tags == tagName
+        testCaseItems[1].tags == tagName
+        testCaseItems.size== 2
     }
 }
